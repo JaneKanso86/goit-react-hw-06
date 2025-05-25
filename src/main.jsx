3d381dfae4b8425f7f585c1404bd4a3c5d0ebe1e
Jane Vasylieva <janekanso86@gmail.com>
2025-05-25 18:14:12 +0300

    add tasks

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.jsx';
-import 'modern-normalize';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 );
