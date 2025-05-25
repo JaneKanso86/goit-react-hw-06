cc28e56f3c305bd03fc69f22b0fd04f183a98067
Jane Kanso <janekanso86@gmail.com>
2025-05-25 11:38:29 +0300

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,29 @@
+import css from './Contact.module.css';
+import { FaUser, FaPhone } from 'react-icons/fa';
+
+export default function Contact({ data: { id, name, number }, onDelete }) {
+  return (
+    <div className={css.card}>
+      <div>
+        <p>
+          <FaUser className={css.icon} />
+          {name}
+        </p>
+        <p>
+          <FaPhone className={css.icon} />
+          {number}
+        </p>
+      </div>
+      <div>
+        <button
+          className={css.button}
+          onClick={() => {
+            onDelete(id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,15 @@
+import css from './SearchBox.module.css';
+
+export default function SearchBox({ value, onFilterChange }) {
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        value={value}
+        className={css.filter}
+        onChange={(e) => onFilterChange(e.target.value)}
+      />
+    </div>
+  );
+}
